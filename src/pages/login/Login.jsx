90d06758647b9420ee7cb383b5753e558440037e
Bappy <bappymahmud321@gmail.com>
2023-05-18 15:34:23 +0600

    added firebase log in

--- src/pages/login/Login.jsx
+++ src/pages/login/Login.jsx
@@ -2,6 +2,9 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 
 const Login = () => {
+    const handleLogIn = event => {
+        event.preventDefault();
+    }
     return (
         <div>
              <div className="hero min-h-screen bg-base-200">
@@ -12,7 +15,7 @@ const Login = () => {
                 <div className="card flex-shrink-0 w-full max-w-sm shadow-2xl bg-base-100">
                     <div className="card-body">
                         <h1 className="text-3xl text-center font-bold">Login </h1>
-                        <form >
+                        <form onSubmit={handleLogIn}>
                             <div className="form-control">
                                 <label className="label">
                                     <span className="label-text">Email</span>
--- src/pages/registration/Registration.jsx
+++ src/pages/registration/Registration.jsx
@@ -2,6 +2,9 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 
 const Registration = () => {
+    const handleregistration = event => {
+        event.preventDefault();
+    }
     return (
         <div className="hero min-h-screen bg-base-200">
             <div className="hero-content flex-col lg:flex-row">
@@ -11,12 +14,18 @@ const Registration = () => {
                 <div className="card flex-shrink-0 w-full max-w-sm shadow-2xl bg-base-100">
                     <div className="card-body">
                         <h1 className="text-3xl text-center font-bold">registration </h1>
-                        <form >
+                        <form onSubmit={handleregistration}>
                             <div className="form-control">
                                 <label className="label">
                                     <span className="label-text">Name</span>
                                 </label>
                                 <input type="text" name='name' placeholder="Name" className="input input-bordered" />
+                            </div>
+                            <div className="form-control">
+                                <label className="label">
+                                    <span className="label-text">Photo Url</span>
+                                </label>
+                                <input type="text" name='photo' placeholder="photo url" className="input input-bordered" required />
                             </div>
                             <div className="form-control">
                                 <label className="label">
@@ -33,6 +42,7 @@ const Registration = () => {
                                     <a href="#" className="label-text-alt link link-hover">Forgot password?</a>
                                 </label>
                             </div>
+
                             <div className="form-control mt-6">
                                 <input className="btn btn-warning" type="submit" value="Sign Up" />
                             </div>
