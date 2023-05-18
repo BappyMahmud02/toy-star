5ca22f797ef34e634c2d9cc01919ed9a912c02e0
Bappy <bappymahmud321@gmail.com>
2023-05-18 11:06:43 +0600

    added navbar

--- src/layout/Main.jsx
+++ src/layout/Main.jsx
@@ -1,12 +1,13 @@
 import React from 'react';
 import { Outlet } from 'react-router-dom';
 import Footer from '../pages/Home/Home/shared/footer/Footer';
+import Navbar from '../pages/Home/Home/shared/navbar/Navbar';
 
 const Main = () => {
     return (
         <div>
+            <Navbar></Navbar>
             <Outlet></Outlet>
-
             <Footer></Footer>
         </div>
     );
